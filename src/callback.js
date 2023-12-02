0e7b646136f90165b848ff45376a8cf487c568c3
maxa27 <makhmudkhamidov5@gmail.com>
2023-12-02 17:38:13 +0300

    updated2 task 1-9

---
+++ src/callback.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import { name, getName } from './cli.js';
+
+export function welcome() {
+  console.log('Welcome to the Brain Games!');
+  getName(name);
+}
+
+export function congratulations() {
+  console.log(`Congratulations, ${name}`);
+}
